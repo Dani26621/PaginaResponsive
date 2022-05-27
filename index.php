b237b65803c263940a36e67eb31a8bb8c456322b
Dani26621 <92678351+Dani26621@users.noreply.github.com>
2022-05-27 16:53:57 +0200

    Aggiornamento

---
+++ index.php
@@ -0,0 +1,41 @@
+<meta name="viewport" content="width=device-width, 
+initial-scale=1.0">  
+
+<html>
+    <head>
+        <title>Hanss-g Bar</title>
+        <link rel="icon" href="https://img.icons8.com/emoji/100/000000/sandwich-emoji.png">
+        <link rel="stylesheet" type="text/css" href="Index.css">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Outfit&display=swap" rel="stylesheet">
+    </head>
+
+    <body>
+
+
+        <form action="register.php" method="POST">
+            <h2>Registrati<h2>
+
+            <p>Email</p>
+            <input type="email" name="email" id ="email" placeholder="Inserisci la tua email" required>
+
+            <p>Username</p>
+            <input type="text" name="username" id ="username" placeholder="Inserisci il tuo username" required>
+
+            <p>Password</p>
+            <input type="password" name="password" id ="password" placeholder="Inserisci la tua password" required>
+
+            <input type="submit" value="invia">
+
+        </form>
+
+    
+        <script>
+        </script>
+
+        <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+        <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+    
+    </body>
+</html>
